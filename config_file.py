5c4e4b27aa8edda8f67b54f8fae2cf2552eeb5ba
Hagai Kraus <krauskas@gmail.com>
2022-10-16 00:46:15 +0300

    removed cached for .gitignore

--- config_file.py
+++
@@ -1,2 +0,0 @@
-API_KEY = "5b0"
-content_path = r'C:\Users\Hagai\Desktop\AWS\study_subjects\python\imdb_ex\posters'
--- dockers_draft/hello.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python
-
-import sys
-
-print("hello there!")
-print(sys.version)
