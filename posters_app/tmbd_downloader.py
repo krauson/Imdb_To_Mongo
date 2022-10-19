c351529f1aa121ad403939ca6322df8249c6187e
Hagai Kraus <krauskas@gmail.com>
2022-10-19 12:53:21 +0300

    fixed a bug in tmdb_downloader

--- posters_app/tmbd_downloader.py
+++ posters_app/tmbd_downloader.py
@@ -4,6 +4,7 @@ import imdb
 import os
 import sys
 
+
 def convert_image_size_to_int(x: str) -> int:
     """sorting function to get the biggest picture size """
     return float("inf") if x == 'original' else int(x[1:])
@@ -20,7 +21,8 @@ class TMDBDownloader:
         self.config = requests.get(self.url).json()
         self.image_base_url = self.config['images']['base_url']
         self.sizes = self.config['images']['poster_sizes']
-        self.max_size = max(self.sizes, key=convert_image_size_to_int)  # use the sort function in max to get biggest size
+        # use the sort function in max to get biggest size
+        self.max_size = max(self.sizes, key=convert_image_size_to_int)
         self.movie_name = movie_name         # check name for legality
         self.imdb_id = self.get_movie_id()
         self.filename = movie_name + '_poster.jpeg'
@@ -40,7 +42,8 @@ class TMDBDownloader:
             print("Refresh and try again in a minute, too frequent API requests")
 
     def get_poster_url(self):
-        img_url_request = self.IMAGE_REQUEST_PATTERN.format(key=self.KEY, imdbid=self.imdb_id)
+        img_url_request = self.IMAGE_REQUEST_PATTERN.format(
+            key=self.KEY, imdbid=self.imdb_id)
         api_response = requests.get(img_url_request).json()
         try:
             # print(f"api_response[posters]:{api_response['posters']}")
@@ -48,7 +51,8 @@ class TMDBDownloader:
             poster_urls = []
             for poster in posters:
                 rel_path = poster['file_path']
-                url = "{0}{1}{2}".format(self.image_base_url, self.max_size, rel_path)
+                url = "{0}{1}{2}".format(
+                    self.image_base_url, self.max_size, rel_path)
                 poster_urls.append(url)
             poster_url = poster_urls[0]
             self.poster_url = poster_url
@@ -59,14 +63,13 @@ class TMDBDownloader:
             print(KeyError)
             return None
 
-
     def download_poster_file(self):
         poster_url = self.get_poster_url()
         poster_data = requests.get(poster_url)
         poster_binary = poster_data.content
 
         print(f"sys.platform:{sys.platform}")
-        target_path = '/'.join([, self.filename])
+        target_path = '/'.join([content_path, self.filename])
         # target_path = '\\'.join([content_path, self.filename])
         print(f"target_path: {target_path}")
         with open(target_path, 'wb') as image_file:
